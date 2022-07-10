ea225dec5ac3071fdff7e7513204c90863757e0a
APIsareLayer8 <orchid1922@duck.com>
2022-07-10 13:12:47 -0400

    Your Virtual Coin - A simple random generator Useful if you are light on coins but need some randomness in your decision.

---
+++ main.py
@@ -0,0 +1,12 @@
+import random
+print("Welcome to Your Virtual Coin!\n")
+
+coin = random.randint(0, 1)
+
+if coin == 0:
+    print("You flipped Heads!\n")
+else:
+    print("You flipped Tails!\n")
+
+print("Did you win? Thank you for using Your Virtual Coin! See you again soon. . .  ")
+
